cd35090fbd6cf8568bb3ed3fbf0cc35cc926bc5b
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2024-11-18 14:42:29 -0700

    Update midi conversion script to go through every midi file in midi-files folder

--- midi-converter/convert.py
+++ midi-converter/convert.py
@@ -1,44 +1,53 @@
 import mido
+import os
 
-mid = mido.MidiFile("midi-files/mongrel.MID")
+midi_dir = os.path.dirname(os.path.realpath(__file__)) + "/midi-files/"
+print(f"Looking for files in directory {midi_dir}")
 
-left_map = []
-right_map = []
-up_map = []
+files = os.listdir(midi_dir)
 
-current_tick = 0
+for file in files:
+    # Skip file if undesired format
+    if not file.endswith(".MID"):
+        continue
 
-# Get tempo
-tempo = 0
-for track in mid.tracks:
-    for msg in track:
-        if msg.type == "set_tempo":
-            tempo = msg.tempo
-            print(f"Set tempo to {tempo}")
+    # Else, run conversion
+    print(f"\nfor MIDI file {file}:")
+    mid = mido.MidiFile("midi-files/mongrel.MID")
+    left_map = []
+    right_map = []
+    up_map = []
+    current_tick = 0
 
-if tempo == 0: tempo = 500_000 # default value of 120bpm
+    # Get tempo
+    tempo = 0
+    for track in mid.tracks:
+        for msg in track:
+            if msg.type == "set_tempo":
+                tempo = msg.tempo
+                print(f"Set tempo to {tempo}")
+    if tempo == 0: tempo = 500_000 # default value of 120bpm
 
-for msg in mid.tracks[0]:
-    print(msg)
-    if msg.type == "note_on":  
-        current_tick += msg.time
-        current_time = mido.tick2second(current_tick, mid.ticks_per_beat, tempo)
-        match(msg.note):
-            case 60:
-                left_map.append(current_time)
-                continue
-            case 61:
-                up_map.append(current_time)
-                continue
-            case 62:
-                right_map.append(current_time)
-                continue
-            case _:
-                print("Invalid note")
-    elif msg.type == "note_off":
-        current_tick += msg.time
-    
-
-print(f"left map: {left_map}")
-print(f"up map: {up_map}")
-print(f"right map: {right_map}")
+    # Change which channel the note tracks to
+    for msg in mid.tracks[0]:
+        if msg.type == "note_on":  
+            current_tick += msg.time
+            current_time = mido.tick2second(current_tick, mid.ticks_per_beat, tempo)
+            match(msg.note):
+                case 60:
+                    left_map.append(current_time)
+                    continue
+                case 61:
+                    up_map.append(current_time)
+                    continue
+                case 62:
+                    right_map.append(current_time)
+                    continue
+                case _:
+                    print("Invalid note")
+        elif msg.type == "note_off":
+            current_tick += msg.time
+        
+    print(f"left map: {left_map}")
+    print(f"up map: {up_map}")
+    print(f"right map: {right_map}")
